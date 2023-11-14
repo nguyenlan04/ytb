d27c5ad09094adf86276e90a7b5b25cce824d6fd
nguyenlan04 <lannt.04.qn@gmail.com>
2023-11-14 22:19:43 +0700

    init project

---
+++ pages/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Web clothes fashion</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+           <!-- <img src="images/banner.jpg" width="100%" height="130px" > -->
+        </div>
+        <div class="menu" >
+            <ul class="list_menu" >
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="#">Danh mục sản phẩm</a></li>
+                <li><a href="#">Giỏ hàng</a></li>
+                <li><a href="#">Tin tức</a></li>
+                <li><a href="#">Liên hệ</a></li>
+            </ul>
+        </div>
+        <div id="main" >
+            <div class="sidebar" >
+                sidebar
+
+            </div>
+            <div class="maincontent" >
+                main content
+
+            </div>
+        </div>
+        <div class="clear" >
+
+        </div>
+        <div class="footer" >
+            <p>footer</p>
+        </div>
+        
+    </div>
+</body>
+</html>
